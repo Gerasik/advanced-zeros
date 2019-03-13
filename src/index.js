0ccfd810bd4d95e894bd058c984765c16bdacaaf
Gerasik <gerasik1992@gmail.com>
2019-03-13 20:35:23 +0300

    complite #1

--- src/index.js
+++ src/index.js
@@ -18,26 +18,22 @@ module.exports = function getZerosCount(number,base){
         i++;
     }};
     for(var j=0; j<resCount.length; j++){
-        maxMin.push(Math.floor(calcAll(number, primitive[j])/resCount[j]));
-        console.log(Math.floor(calcAll(number, primitive[j])/resCount[j]));
-    } ;
-    maxMin.push(calcAll(number, 5));    
+        maxMin.push(Math.floor(calcInkl(number, primitive[j])/resCount[j]));
+    } ;   
     var result = maxMin[0];
     for(var j=0; j<maxMin.length;j++){
         result = Math.min(maxMin[j], result);
     };
-    console.log(primitive);
-    console.log(resCount);
     return result;
 }
 
 
-function calcAll(number, base){
+function calcInkl(number, base){
     var result = 0;
+    var baseClone = base;
     while(base <= number){
         result += Math.floor(number/base);
-        console.log('base: '+base+', number: '+ number+', result: '+ result);
-        base *= base;
+        base *= baseClone;
     } 
     return result;
 };
